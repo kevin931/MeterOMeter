6ca9dfc8046d1a3b23a0bf2bd25c5882672d7b50
Kevin Wang <kevinwang@smu.edu>
2018-12-14 11:46:39 -0600

    Added support for meterometer.com YAML output format for up to four different pronunciations for each word; testing word boundary

--- R/meter_score.R
+++ R/meter_score.R
@@ -12,7 +12,11 @@ meter_score<-function(x,
                                       "hexameter",
                                       "heptameter",
                                       "octameter"),
-                       secondary_stress=T
+                       secondary_stress=T,
+
+                      word_boundary=F,
+
+                      meterometer.com=F
                        )
 {
   if (meter_length=="dimeter") {
@@ -74,36 +78,212 @@ meter_score<-function(x,
 
   pattern_length<-nchar(pattern)
 
-  x<-paste(x, collapse="")
+  rm(meter_length, meter_type)
 
-  x<-unlist(strsplit(x, ""))
+  if (meterometer.com) {
 
-  if (secondary_stress) {
+    pronunciation.1<-as.character(x$pronunciation.1)
 
-    x[which(x=="2")]<-"1"
+    pronunciation.2<-as.character(x$pronunciation.2)
+
+    pronunciation.3<-as.character(x$pronunciation.3)
+
+    pronunciation.4<-as.character(x$pronunciation.4)
+
+    pronunciation.2[which(is.na(pronunciation.2))]<-pronunciation.1[which(is.na(pronunciation.2))]
+
+    pronunciation.3[which(is.na(pronunciation.3))]<-pronunciation.1[which(is.na(pronunciation.3))]
+
+    pronunciation.4[which(is.na(pronunciation.4))]<-pronunciation.1[which(is.na(pronunciation.4))]
+
+    pronunciation.1<-paste(pronunciation.1, collapse = "")
+
+    pronunciation.1<-unlist(strsplit(pronunciation.1, ""))
+
+    pronunciation.2<-paste(pronunciation.2, collapse = "")
+
+    pronunciation.2<-unlist(strsplit(pronunciation.2, ""))
+
+    pronunciation.3<-paste(pronunciation.3, collapse = "")
+
+    pronunciation.3<-unlist(strsplit(pronunciation.3, ""))
+
+    pronunciation.4<-paste(pronunciation.4, collapse = "")
+
+    pronunciation.4<-unlist(strsplit(pronunciation.4, ""))
+
+    index.1<-vector()
+
+    index.2<-vector()
+
+    index.3<-vector()
+
+    index.4<-vector()
+
+    for (i in 1:(length(pronunciation.1)-pattern_length+1)) {
+
+      portion.1<-paste(pronunciation.1[i:(i+pattern_length-1)], collapse = "")
+
+      portion.2<-paste(pronunciation.2[i:(i+pattern_length-1)], collapse = "")
+
+      portion.3<-paste(pronunciation.3[i:(i+pattern_length-1)], collapse = "")
+
+      portion.4<-paste(pronunciation.4[i:(i+pattern_length-1)], collapse = "")
+
+      if (portion.1==pattern) {
+
+        index.1[i]<-1
+      } else {
+
+        index.1[i]<-0
+      }
+
+      if (portion.2==pattern) {
+
+        index.2[i]<-1
+      } else {
+
+        index.2[i]<-0
+      }
+
+      if (portion.3==pattern) {
+
+        index.3[i]<-1
+      } else {
+
+        index.3[i]<-0
+      }
+
+      if (portion.4==pattern) {
+
+        index.4[i]<-1
+      } else {
+
+        index.4[i]<-0
+      }
+
+    }
+
+    index<-index.1+index.2+index.3+index.4
+
+    for (i in 1:length(index)) {
+
+      if (index[i]==0) {
+
+        index[i]<-0
+
+      }  else {
+
+        index[i]<-1
+      }
+
+    }
+
+    rm(portion.1, portion.2, portion.3, portion.4,
+       pronunciation.1, pronunciation.2, pronunciation.3,
+       pronunciation.4)
+
+    if (word_boundary) {
+
+      warning("word_boundary not yet available.
+              Stay tuned for future updates.")
+
+      if (1==0) {
+
+        word.index<-rep(0, nrow(x))
+
+        syl.start.1<-vector()
+
+        syl.end.1<-vector()
+
+        syllables.1<-x$syllables.1
+
+        for (i in 1:nrow(x)) {
+
+          if (i==1) {
+
+            syl.end.1[i]<-syllables.1[i]
+
+            syl.start.1[i]<- 1
+
+          }  else {
+
+            syl.end.1[i]<-sum(syllables.1[i], syl.end.1[i-1])
+
+            syl.start.1[i]<- syl.end.1[i]-syllables.1[i]+1
+
+          }
+
+        }
+
+
+        index.1.syl<-index.1.syl[which(index.1==1)]
+
+        length.syl.start<-length(syl.start.1)
+
+        length.syl.end<-length(syl.end.1)
+
+        for (i in 1:length(index.1.syl)) {
+
+          print(i)
+
+          instance<-unlist(strsplit(index.1.syl[i], split = "_"))
+
+          if (length(setdiff(syl.start.1, instance[1]))<length.syl.start &
+              length(setdiff(syl.end.1, instance[2]))<length.syl.end ) {
+
+            word.index[which(syl.start.1==instance[1]):which(syl.end.1==instance[2])]<-1
+
+          }
+
+        }
+
+      }
+
+
+      }
+
+
+    metrical_score<-sum(index)/length(index)
+
+
 
   } else {
 
-    x[which(x=="2")]<-"0"
+    x<-paste(x, collapse="")
 
-  }
+    x<-unlist(strsplit(x, ""))
 
-  index<-vector()
+    if (secondary_stress) {
 
-  for (i in 1:(length(x)-pattern_length+1)) {
+      x[which(x=="2")]<-"1"
 
-    portion<-paste(x[i:(i+pattern_length-1)], collapse = "")
-
-    if (portion==pattern) {
-
-      index[i]<-1
     } else {
 
-      index[i]<-0
+      x[which(x=="2")]<-"0"
+
     }
+
+    index<-vector()
+
+    for (i in 1:(length(x)-pattern_length+1)) {
+
+      portion<-paste(x[i:(i+pattern_length-1)], collapse = "")
+
+      if (portion==pattern) {
+
+        index[i]<-1
+      } else {
+
+        index[i]<-0
+      }
+    }
+
+    metrical_score<-sum(index)/length(index)
   }
 
-  metrical_score<-sum(index)/length(index)
+
+
 
   return(metrical_score)
 
